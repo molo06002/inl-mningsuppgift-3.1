bb0435a621439763192e2748adb4b0370c80af4e
molo06002 <molo06002@E-PW0234KQ>
2023-09-29 10:37:59 +0200

    Add project files.

---
+++ inlämningsuppgift 3.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace inlämningsuppgift1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till tekikums tävling. För att du ska kunna delta måste du vara 16 - 19 år gammal.");
+            Console.WriteLine("Skriv in ditt ålder här: ");
+            string x = Console.ReadLine();
+            int y = Convert.ToInt32(x);
+            Console.Clear();
+            switch (y)
+            {
+                case 16:
+                    Console.WriteLine("Du är 16 år gammal, du får delta i tävlingen");
+                    break;
+
+                case 17:
+                    Console.WriteLine("Du är 17 år gammal, du får delta i tävlingen");
+                    break;
+
+                case 18:
+                    Console.WriteLine("Du är 18 år gammal, du får delta i tävlingen");
+                    break;
+
+                case 19:
+                    Console.WriteLine("Du är 19 år gammal, du får delta i tävlingen");
+                    break;
+
+                default:
+                    Console.WriteLine("Du är antingen över 19 år gammal eller under 16, du får inte delta i tävlingen");
+                    break;
+            }
+
+        }
+    }
+}
